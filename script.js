c7803931980bdb7458afea475b7d05b4bf604d98
INll <waileungsunny@gmail.com>
2022-04-04 00:05:36 +0800

    Complete ui (wip)

--- script.js
+++ script.js
@@ -1,7 +1,71 @@
+// Element selection
+const resultText = document.querySelector('.results');
+const scoreText = document.querySelector('.scoreboard');
+const finalResultText = document.querySelector('.finalResult');
+const scores = document.querySelectorAll('.score');
+
 // Calculate computer choice
 function computerPlay () {
   let ranNum = Math.floor(Math.random() * 3);
   return (ranNum === 0) ? 'rock' : (ranNum === 1) ? 'paper' : 'scissors';
+}
+
+// Reset game
+function resetGame () {
+  if (gameEnded) {
+    computerScore = 0;
+    humanScore = 0;
+    gameEnded = false;
+    scores.forEach(score => {
+      score.textContent = 0;
+    })
+    finalResultText.textContent = '';
+    resetButton.style.display = "block";
+    const resultBanner = document.querySelector(".resultBanner");
+    resultBanner.style.backgroundColor = '#1a1a1d';
+    resultText.textContent = '';
+    resetButton.style.display = 'none';
+  }
+}
+
+// Remove trnasition based on applied transition class
+function removeTransition (e) {
+  console.log('removing transition');
+  console.log(e.target.classList[1]);
+  if (e.propertyName !== 'background-color') return;
+  switch (e.target.classList[1]) {
+    case 'loss-bkgd':
+      e.target.classList.remove('loss-bkgd');
+      console.log('removing red background');
+    case 'win-bkgd':
+      e.target.classList.remove('win-bkgd');
+      console.log('removing green background');
+    case 'tie-bkgd':
+      e.target.classList.remove('tie-bkgd');
+      console.log('removing grey background');
+  }
+}
+
+// Change result banner background color depending on results
+function roundResultTransition (roundResult) {
+  const resultBanner = document.querySelector(".resultBanner");
+  switch (roundResult) {
+    case 'tie':
+    resultBanner.style.backgroundColor = '#333333';
+    // resultBanner.classList.add('tie-bkgd');
+    console.log('made background grey');
+    break;
+    case 'win':
+    resultBanner.style.backgroundColor = '#058224';
+    // resultBanner.classList.add('win-bkgd');
+    console.log('made background green');
+    break;
+    case 'loss':
+    resultBanner.style.backgroundColor = '#820505';
+    // resultBanner.classList.add('loss-bkgd');
+    console.log('made background red');
+    break;
+  }
 }
 
 // Compare selections and determine winner
@@ -9,6 +73,7 @@ function playRound (playerSelection, computerSelection) {
 
   // Handle tied 
   if (playerSelection === computerSelection) {
+    roundResultTransition('tie');
     return 'Tie'
   } else {
     let results = playerSelection + computerSelection;
@@ -19,23 +84,27 @@ function playRound (playerSelection, computerSelection) {
       // All wins
       case 'scissorspaper':
         humanScore++;
+        roundResultTransition('win');
         return `You Win! Scissors beat paper.`
       case 'paperrock':
       case 'rockscissors':
         cappedSelection = playerSelection.slice(0, 1).toUpperCase() + 
             playerSelection.slice(1);
         humanScore++;
+        roundResultTransition('win');
         return `You Win! ${cappedSelection} beats ${computerSelection}.`
         
       // All losses
       case 'paperscissors':
         computerScore++;
+        roundResultTransition('loss');
         return `You Lose! Scissors beat paper.`
       case 'scissorsrock':
       case 'rockpaper':
         cappedSelection = computerSelection.slice(0, 1).toUpperCase() + 
             computerSelection.slice(1);
         computerScore++;
+        roundResultTransition('loss');
         return `You Lose! ${cappedSelection} beats ${playerSelection}.`
       default:
         return `Error: Invalid input.`  // Invalid input
@@ -52,6 +121,10 @@ const warningText = document.createElement('div');
 let playingToForm = document.querySelector(".roundInputForm");
 window.onload = () => {
   playingToForm.addEventListener('input', () => {
+    if (gameEnded) {
+      resetGame();
+      return;
+    }
     maxScore = playingToForm.value;
     // Check invalid input
     if (!(Number(maxScore) > 0 && Number(maxScore) < 101)) {
@@ -73,12 +146,6 @@ window.onload = () => {
 let computerScore = 0;
 let humanScore = 0;
 let gameEnded = false;
-
-// Element selection
-const resultText = document.querySelector('.results');
-const scoreText = document.querySelector('.scoreboard');
-const finalResultText = document.querySelector('.finalResult');
-const scores = document.querySelectorAll('.score');
 
 // Execute game
 function game() {
@@ -102,15 +169,15 @@ function game() {
     resultText.textContent = playRound(playerSelection, computerSelection);
     // Update scores
     scores.forEach(score => {
-      console.log(score.className);
+      // console.log(score.className);
       switch (score.className) {
         case 'human score':
           score.textContent = humanScore;
-          console.log(`Human score updated to ${humanScore}.`)
+          // console.log(`Human score updated to ${humanScore}.`)
           break;
         case 'computer score':
           score.textContent = computerScore;
-          console.log(`Computer score updated to ${computerScore}.`)
+          // console.log(`Computer score updated to ${computerScore}.`)
           break;
       }
     })
@@ -118,11 +185,14 @@ function game() {
   // Check for end-game
   if (humanScore >= maxScore || computerScore >= maxScore) {
     if (humanScore > computerScore) {
-      finalResultText.textContent = 'You Win!';
+      resultBanner.style.backgroundColor = '#058224';
+      resultText.textContent = 'You Win!';
     } else if (computerScore > humanScore) {
-      finalResultText.textContent = 'You Lost! Good luck next time.';
+      resultBanner.style.backgroundColor = '#820505';
+      resultText.textContent = 'You Lost! Good luck next time.';
     } else {
-      finalResultText.textContent = 'Tie!';
+      resultBanner.style.backgroundColor = '#333333';
+      resultText.textContent = 'Tie!';
     }
     gameEnded = true;
     // Show reset button and allow for reset
@@ -130,19 +200,10 @@ function game() {
   }
 }
 
-// Reset everything
+// Functionality of reset button
 const resetButton = document.querySelector('.resetButton');
 resetButton.addEventListener('click', () => {
-  if (gameEnded) {
-    console.log('lol');
-    computerScore = 0;
-    humanScore = 0;
-    gameEnded = false;
-    scores.forEach(score => {
-      score.textContent = 0;
-    })
-    finalResultText.textContent = '';
-  }
+  resetGame();
 })
 
 const buttons = document.querySelectorAll('.choice');
@@ -150,14 +211,9 @@ buttons.forEach(button => {
   button.addEventListener('click', game);
 })
 
-// Game execution  --  Play five rounds
-// function game () {
-//   for (let i = 0; i < 5; i++) {
-//     let playerSelection = 
-//         window.prompt("Rock, Paper or Scissors?").toLowerCase();
-//     let computerSelection = computerPlay();
-//     console.log(playRound(playerSelection, computerSelection));
-//   }
-// }
+// Listen for end of result banner transitions
+const resultBanner = document.querySelector(".resultBanner");
+resultBanner.addEventListener('transitionend', removeTransition);
 
-// game();  // Execute the game
+// How to remove transition based on transition event class name?
+// How to make it fade-out, not fade-in?
